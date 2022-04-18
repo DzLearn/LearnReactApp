ff4f8d142151a66a0b03500ec19c24e7adb1cdfd
SaadDamine <m.sdaminesaad@gmail.com>
2022-04-18 15:24:09 +0100

    Update Home.js

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,28 +1,12 @@
-import React, { useState } from 'react'
+import React from 'react'
 
 import Button from 'react-bootstrap/Button'
 
 export default function Home()
 {
-  const [count, setCount] = useState( "true" );
- 
-
-  const handleClick = () =>
-  {
-    if ( count === "true" )
-    {
-      setCount( "false" )
-    } else
-    {
-      setCount( "true" )
-    }
-    
-  };
   return (
     <div>
-      <Button variant="primary" onClick={handleClick}>Primary</Button>
-      <hr/>
-      <h1>this is {count}</h1>
+      Home
     </div>
     
   )
