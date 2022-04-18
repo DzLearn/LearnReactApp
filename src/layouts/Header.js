b424b2a7cefc483f357430d24b0e1af942884388
SaadDamine <m.sdaminesaad@gmail.com>
2022-04-18 13:47:34 +0100

    include React Bootstrap in project
    React-Bootstrap is a component-based library that provides native Bootstrap components as pure React components. Instead of utilizing JavaScript source and plugins from the CDN, it converts all JavaScript to React and bundles all components together.

--- src/layouts/Header.js
+++ src/layouts/Header.js
@@ -1,13 +1,25 @@
 import React from 'react'
 import { Link } from "react-router-dom";
+import { Container, Nav, Navbar } from "react-bootstrap";
+
+const linkStyle = {
+  margin: "1rem",
+  textDecoration: "none",
+  color: 'white'
+};
 
 export default function Header() {
   return (
-    <div>
-      <nav>
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
-      </nav>
-    </div>
+    <>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link><Link to="/" style={ linkStyle }>Home</Link></Nav.Link>
+            <Nav.Link><Link to="/about" style={ linkStyle }>About</Link></Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </>
   )
 }
