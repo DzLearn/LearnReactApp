8a993fa4f582eb8cdfda4e562104e63019a2b615
SaadDamine <m.sdaminesaad@gmail.com>
2022-04-19 11:55:10 +0100

    Awesome Expandable Card Animation With Framer Motion

--- src/pages/ReadMore.js
+++ src/pages/ReadMore.js
@@ -4,6 +4,10 @@ import { Link } from "react-router-dom";
 import { useParams } from "react-router-dom";
 import { Card, Button } from 'react-bootstrap'
 
+import { motion } from 'framer-motion';
+
+import '../assets/styles/more.css'
+
 const linkStyle = {
     margin: "1rem",
     textDecoration: "none",
@@ -24,19 +28,62 @@ export default function ReadMore(props)
 
     return (
         <div className='container'>
-            <Card className="text-center">
-                <Card.Header>Featured</Card.Header>
-                <Card.Body>
-                    <Card.Title>{data.title}</Card.Title>
-                    <Card.Text>
-                        {data.body}
-                    </Card.Text>
-                    <Button variant="primary">
-                        <Link to="/" style={linkStyle}>Return Home</Link>
-                    </Button>
-                </Card.Body>
-                <Card.Footer className="text-muted">{data.userId} days ago</Card.Footer>
-            </Card>
+            <NavCard data={data} />
+            <AnimationCard data={data}/>
         </div>
     )
+}
+
+function NavCard(props)
+{
+    return (
+        <Card className="text-center">
+            <Card.Header>Featured</Card.Header>
+            <Card.Body>
+                <Card.Title>{props.data.title}</Card.Title>
+                <Card.Text>
+                    {props.data.body}
+                </Card.Text>
+                <Button variant="primary">
+                    <Link to="/" style={linkStyle}>Return Home</Link>
+                </Button>
+            </Card.Body>
+            <Card.Footer className="text-muted">{props.data.userId} days ago</Card.Footer>
+        </Card>
+    );
+}
+
+function AnimationCard( props )
+{
+    const [isOpen, setIsOpen] = useState( false );
+    return (
+        <div className='main'>
+            <motion.div
+                style={{
+                    borderRadius: "2rem",
+                    boxShadow:"0px 10px 30px rgba(0,0,0,0.5)"
+                }}
+                layout
+                transition={{layout:{duration:1,type:"spring"}}}
+                onClick={() => { setIsOpen( !isOpen ) }} className='mcard'>
+                <motion.h2 layout="position">{props.data.title}</motion.h2>
+                {isOpen && (
+                    ( <>
+                        <motion.div
+                            initial={{ opacity: 0 }}
+                            animate={{ opacity: 1 }}
+                            transition={{duration:1}}
+                            className='expand'
+                        >
+                            <p>{props.data.body}</p>
+                        </motion.div>
+                        <Button variant="primary">
+                            <Link to="/" style={linkStyle}>Return Home</Link>
+                        </Button>
+                   </>)
+                )}
+                
+                
+            </motion.div>
+        </div> );
 }
